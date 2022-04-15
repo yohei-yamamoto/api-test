58fd9ec66c79f36635567d828b8bf27fb8fcad60
Yohei Yamamoto <yohei_yamamoto@dwango.co.jp>
2022-04-15 17:31:39 +0900

    table タグに css を設定して見た目を調整した

--- src/components/readJson.js
+++ src/components/readJson.js
@@ -19,16 +19,16 @@ const ReadJson = () => {
           items.map((item)  => {
             return (
               <>
-              <tr><td>団体コード</td><td>{item["団体コード"]}</td></tr>
-              <tr><td>団体名</td><td>{item["団体名"]}</td></tr>
-              <tr><td>名称</td><td>{item["名称"]}</td></tr>
-              <tr><td>概要文（補足説明）</td><td>{item["概要文（補足説明）"]}</td></tr>
-              <tr><td>区分</td><td>{item["区分"]}</td></tr>
-              <tr><td>場所_住所</td><td>{item["場所_住所"]}</td></tr>
-              <tr><td>場所_緯度</td><td>{item["場所_緯度"]}</td></tr>
-              <tr><td>場所_経度</td><td>{item["場所_経度"]}</td></tr>
-              <tr><td>アクセス情報</td><td>{item["アクセス情報"]}</td></tr>
-              <tr><td>駐車場情報</td><td>{item["駐車場情報"]}</td></tr>
+              <tr><th>団体コード</th><td>{item["団体コード"]}</td></tr>
+              <tr><th>団体名</th><td>{item["団体名"]}</td></tr>
+              <tr><th>名称</th><td>{item["名称"]}</td></tr>
+              <tr><th>概要文（補足説明）</th><td>{item["概要文（補足説明）"]}</td></tr>
+              <tr><th>区分</th><td>{item["区分"]}</td></tr>
+              <tr><th>場所_住所</th><td>{item["場所_住所"]}</td></tr>
+              <tr><th>場所_緯度</th><td>{item["場所_緯度"]}</td></tr>
+              <tr><th>場所_経度</th><td>{item["場所_経度"]}</td></tr>
+              <tr><th>アクセス情報</th><td>{item["アクセス情報"]}</td></tr>
+              <tr><th>駐車場情報</th><td>{item["駐車場情報"]}</td></tr>
               </>
             )
 /*
